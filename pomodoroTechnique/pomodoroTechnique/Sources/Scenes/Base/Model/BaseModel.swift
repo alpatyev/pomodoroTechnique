e9f9531ed0121862bd678374ddf8e1c05607556a
AlpatyevNikita <chococacaococonut@gmail.com>
2023-02-12 21:21:40 +0300

    reorganized project for MVP-design pattern

---
+++ pomodoroTechnique/pomodoroTechnique/Sources/Scenes/Base/Model/BaseModel.swift
@@ -0,0 +1,8 @@
+//
+//  BaseModel.swift
+//  pomodoroTechnique
+//
+//  Created by Nikita Alpatiev on 2/12/23.
+//
+
+struct TimerModel {}
---
+++ pomodoroTechnique/pomodoroTechnique/Sources/Scenes/Base/Presenter/BasePresenter.swift
@@ -0,0 +1,14 @@
+//
+//  BasePresenter.swift
+//  pomodoroTechnique
+//
+//  Created by Nikita Alpatiev on 2/12/23.
+//
+
+// MARK: - Presenter protocol
+
+protocol BasePresenterProtocol {}
+
+// MARK: - Presenter class
+
+final class BasePresenter: BasePresenterProtocol {}
